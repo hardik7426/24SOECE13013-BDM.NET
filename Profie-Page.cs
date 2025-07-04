54ad2c3cec70bbc6c51cdee2f00c181f5cc9fc61
hardik7426 <hardikparekh7426@gmail.com>
2025-07-04 11:44:06 +0530

    Profile-Page

---
+++ Profie-Page.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Name : Parekh Hardik ");
+            Console.WriteLine(" DOB: 7/4/2006 ");
+            Console.WriteLine(" Address: 6, abc society , Kalawad Road ");
+            Console.WriteLine(" City: Rajkot ");
+            Console.WriteLine(" Pincode: 360001 ");
+            Console.WriteLine(" State: Gujarat ");
+            Console.WriteLine(" Country: India ");
+            Console.WriteLine(" Email: hardikparekh7426@gmail.com ");
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
