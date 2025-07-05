8128c9026e7f33992f6454d578194f972d5d040a
hardik7426 <hardikparekh7426@gmail.com>
2025-07-05 17:58:14 +0530

    re-arrange-code-sequence

---
+++ re-arrange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string str;
+
+            Console.WriteLine("Enter Number: ");
+            str = Console.ReadLine();
+
+            x = Convert.ToInt32(str);
+
+            if (x % 2 == 0)
+                Console.WriteLine("Number is Even");
+            else
+                Console.WriteLine("Number is Odd");
+            Console.Read();
+        }
+    }
+}
