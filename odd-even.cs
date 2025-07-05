5678a5e51f21b013784609d10b1b74b01a0a1902
hardik7426 <hardikparekh7426@gmail.com>
2025-07-05 17:49:54 +0530

    odd-even

---
+++ odd-even.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is an Even number");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is an Odd number");
+            }
+                Console.Read();
+        }
+    }
+}
