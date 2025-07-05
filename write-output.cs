06e4adb5e8bed28fac2475e950744cc39bd7df7e
hardik7426 <hardikparekh7426@gmail.com>
2025-07-05 18:10:44 +0530

    write-output-this-code

---
+++ write-output.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fact = 1; // Declaring integer variables 'n' for input number and 'fact' initialized to 1 for storing factorial
+
+            Console.WriteLine("Enter Number : "); // Asking the user to enter a number
+
+            string str = Console.ReadLine(); // Reading user input as string
+
+            n = Convert.ToInt32(str); // Converting the input string to integer and storing in 'n'
+
+            for (int i = 1; i <= n; i++) // Loop from 1 to 'n' to calculate factorial
+            {
+                fact = fact * i; // Multiply 'fact' by current value of 'i' to accumulate factorial
+            }
+
+            Console.WriteLine("Factorial : {0}", fact); // Print the factorial using placeholder syntax
+            Console.Read();
+        }
+    }
+}
