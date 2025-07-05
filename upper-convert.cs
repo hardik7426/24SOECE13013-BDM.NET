3d6274f772cc5c4cf958f5d61c8c082bc8ee25b2
hardik7426 <hardikparekh7426@gmail.com>
2025-07-05 22:02:54 +0530

    UpperCase-convert

---
+++ upper-convert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, upperName;
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            upperName = name.ToUpper();
+
+            Console.WriteLine("OUTPUT: " + upperName);
+
+            Console.ReadLine();
+        }
+    }
+}
