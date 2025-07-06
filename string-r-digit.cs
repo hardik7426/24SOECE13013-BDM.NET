697b9891d01f1c4070ca03f7467894a610c01e83
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 17:40:46 +0530

    string-r-digit

---
+++ string-r-digit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter mobile number: ");
+            string mobile = Console.ReadLine();
+
+            if (mobile.Length >= 5)
+            {
+                string masked = mobile.Substring(0, mobile.Length - 5) + new string('X', 5);
+                Console.WriteLine("Masked Number: " + masked);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+        }
+    }
+}
