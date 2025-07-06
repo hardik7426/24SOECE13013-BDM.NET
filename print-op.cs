a8429a5ef152d870fb7eef4458a0e73541562bcb
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 17:54:19 +0530

    print-o/p

---
+++ print-op.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write(a + " " + b + " ");
+            while (true)
+            {
+                c = a + b;
+                if (c > 55)
+                    break;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
