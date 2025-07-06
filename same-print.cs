8afed7537a6103151945748fa2a3ea656cdb5ce5
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 17:49:22 +0530

    same-print-string

---
+++ same-print.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Name: " + name);
+        }
+    }
+}
