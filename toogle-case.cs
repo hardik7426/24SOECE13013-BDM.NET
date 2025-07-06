36ddaa06c84fcae936e920f2c834c00f8ae30f9f
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 17:35:01 +0530

    toogle-case

---
+++ toogle-case.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string input = Console.ReadLine();
+            string result = "";
+
+            foreach (char ch in input)
+            {
+                if (char.IsUpper(ch))
+                    result += char.ToLower(ch);
+                else if (char.IsLower(ch))
+                    result += char.ToUpper(ch);
+                else
+                    result += ch;
+            }
+
+            Console.WriteLine("Toggle Case: " + result);
+        }
+    }
+}
