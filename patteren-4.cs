6bed6206b2a19f26b9bb20ccf352512fd416e013
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 18:43:55 +0530

    pattteren-4

---
+++ patteren-4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 4;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                    Console.Write(" ");
+                for (int k = 1; k <= i; k++)
+                    Console.Write("* ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
