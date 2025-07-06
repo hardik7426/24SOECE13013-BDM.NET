15ce5bc112d5dde141b740f5120396a1ce737675
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 18:08:18 +0530

    Armstrong--NO

---
+++ Armstrong.NO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int temp = num, sum = 0;
+            int digits = num.ToString().Length;
+
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += (int)Math.Pow(digit, digits);
+                temp /= 10;
+            }
+
+            if (sum == num)
+                Console.WriteLine("Armstrong Number");
+            else
+                Console.WriteLine("Not an Armstrong Number");
+        }
+    }
+}
