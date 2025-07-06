406484ab5190c1e82b7edd545593c19ba300feb0
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 18:54:20 +0530

    sum-of-no

---
+++ sum-of-no.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of terms: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Input number: ");
+            int digit = int.Parse(Console.ReadLine());
+
+            int term = 0, sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                term = term * 10 + digit;
+                Console.Write(term);
+                if (i != n)
+                    Console.Write(" + ");
+                sum += term;
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Sum is: " + sum);
+        }
+    }
+}
