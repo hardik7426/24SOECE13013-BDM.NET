4b4e09216424d38d40ab3806ddcf17de9b9f5482
hardik7426 <hardikparekh7426@gmail.com>
2025-07-06 17:45:19 +0530

    name-t-gendar

---
+++ name-t-gendar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter gender (M/F): ");
+            char gender = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if (gender == 'M')
+                Console.WriteLine("Mr. " + name);
+            else if (gender == 'F')
+                Console.WriteLine("Ms. " + name);
+            else
+                Console.WriteLine("Invalid gender");
+        }
+    }
+}
